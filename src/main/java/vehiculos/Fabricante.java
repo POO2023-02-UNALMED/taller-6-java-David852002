4ff36006ed0ac2f7ebbba1cb9af6568cc2b82785
David852002 <ddelgadoo@unal.edu.co>
2023-10-02 22:55:17 -0500

    Update Fabricante.java

--- src/main/java/vehiculos/Fabricante.java
+++ src/main/java/vehiculos/Fabricante.java
@@ -1,42 +1,46 @@
 package vehiculos;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Fabricante {
 	
 	private String nombre;
 	private País pais;
-	
+	private List<Fabricante> listado = new ArrayList<>();
+	private int fabricanteMasRepetido;
 	
 	public Fabricante(String nombre, País pais) {
 		this.nombre = nombre;
 		this.pais = pais;
+		listado.add(this);
 		
 	}
 	
 	public Fabricante fabricaMayorVentas() {
 		
 		int contador = 0;
-		HashMap<País, Integer> paises = new HashMap<>();
-		for(País x : listado) {
-			if(paises.containsKey(x)) {
+		HashMap<Fabricante, Integer> fabricantes = new HashMap<>();
+		for(Fabricante x : listado) {
+			if(fabricantes.containsKey(x)) {
 				contador++;
 			}
 			else{
-				paises.put(x,1);
+				fabricantes.put(x,1);
 			}
 			
 			
 			int repeticiones = 0;
-			for(País l : paises.keySet()) {
-				if(repeticiones<paises.get(l)) {
-					paisMasRepetido = l;
-					repeticiones = paises.get(l);
+			for(Fabricante l : fabricantes.keySet()) {
+				if(repeticiones<fabricantes.get(l)) {
+					fabricanteMasRepetido = l;
+					repeticiones = fabricantes.get(l);
 				}
 			}
 		}
 		
-		return paisMasRepetido;
+		return fabricanteMasRepetido;
 	}
 	
 	
