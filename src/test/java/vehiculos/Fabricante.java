57f186ad69a8c3bd408a60c6707faa5d37297419
David852002 <ddelgadoo@unal.edu.co>
2023-10-02 22:46:38 -0500

    Update Fabricante.java

--- src/test/java/vehiculos/Fabricante.java
+++ src/test/java/vehiculos/Fabricante.java
@@ -1,13 +1,17 @@
 package vehiculos;
 
+
+
 public class Fabricante {
 	
 	private String nombre;
 	private País pais;
 	
+	
 	public Fabricante(String nombre, País pais) {
 		this.nombre = nombre;
 		this.pais = pais;
+		
 	}
 	
 	public String getNombre() {
