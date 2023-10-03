6199605ec597cc20abd0b57151ec6e4ea1f3be1d
David852002 <ddelgadoo@unal.edu.co>
2023-10-02 23:58:45 -0500

    Update Fabricante.java

--- src/main/java/vehiculos/Fabricante.java
+++ src/main/java/vehiculos/Fabricante.java
@@ -5,11 +5,11 @@ import java.util.HashMap;
 
 public class Fabricante {
 	private String nombre;
-	private Pais pais;
+	private País pais;
 	private static HashMap<Fabricante, Integer> contador = new HashMap<>();
 	
 	
-	public Fabricante(String nombre, Pais pais) {
+	public Fabricante(String nombre, País pais) {
 		this.nombre = nombre;
 		this.pais = pais;
 	}
@@ -22,18 +22,18 @@ public class Fabricante {
 		this.nombre = nombre;
 	}
 	
-	public Pais getPais() {
+	public País getPais() {
 		return pais;
 	}
 	
-	public void setPais(Pais pais) {
+	public void setPais(País pais) {
 		this.pais = pais;
 	}
 	
 	public static void incrementarContador(Fabricante fabricante) {
 		if (contador.containsKey(fabricante)) {
 			contador.put(fabricante, contador.get(fabricante) + 1);
-			Pais.incrementarContador(fabricante.getPais());
+			País.incrementarContador(fabricante.getPais());
 	    } else {
 	    	contador.put(fabricante, 1);
 	    }
